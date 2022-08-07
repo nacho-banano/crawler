8876c4eaf7fe40fc7200b7cfd176dc279173f28b
Hollywood <44089845+Vitali-Lupusor@users.noreply.github.com>
2022-08-07 15:15:55 +0100

    added: can optionally provide a list of pairing as parameters to the `script_download.py` file

--- script_download.py
+++ script_download.py
@@ -1,30 +1,36 @@
 """Script for downloading files from binance vision."""
 
+import argparse
 import asyncio
 import os
 import sys
 from json import load
 from logging import getLogger, Logger
-from typing import Dict, List
+from typing import Collection, Dict, List
 
 from crawler.binance.downloader import download
 
 from consts import LIST_OF_TRIANGLES_PATH, DATA_DIRECTORY
 
-# get our input file
-# a location as we will ls this directory
+logger: Logger = getLogger(__name__)
 
-if __name__ == "__main__":
-    logger: Logger = getLogger(__name__)
 
+async def download_cli(downloader_input: Collection[str]) -> None:
+    """
+    Download the binance data.
+
+    This is a wrapper around the ``download`` function that allows filtering
+    of the desired coin pairings from the total of pairs that are due to be
+    downloaded.
+
+    Parameters
+    ----------
+    downloader_input : Collection[str]
+        TODO: Add description.
+    """
     logger.debug("Creating a list of existing files ...")
     directory_contents: List[str] = os.listdir(DATA_DIRECTORY)
     logger.debug("Done")
-
-    logger.debug("Loading the files of all files ...")
-    with open(LIST_OF_TRIANGLES_PATH, "r", encoding="UTF-8") as file:
-        downloader_input: Dict[str, List[str]] = load(file)
-        logger.debug("Done")
 
     logger.debug(
         "Comparing the list of existing files against the full list ..."
@@ -38,9 +44,37 @@ if __name__ == "__main__":
         ]
     logger.debug("List generated. Ready to start downloading.")
 
+    logger.debug("Download started")
+    await download(downloader_input, DATA_DIRECTORY)
+    logger.debug("Download finished")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Binance data downloader")
+
+    parser.add_argument(
+        "downloader_input",
+        action="store",
+        type=str,
+        nargs="*",
+    )
+
+    logger.debug("Loading the list of all files ...")
+    with open(LIST_OF_TRIANGLES_PATH, "r", encoding="UTF-8") as file:
+        pairs_list: Dict[str, List[str]] = load(file)
+        logger.debug("Done")
+
+    namespace = parser.parse_args()
+
+    if namespace.downloader_input:
+        pairs_list = {
+            key: value
+            for key, value in pairs_list.items()
+            if key in namespace.downloader_input
+        }
+
     try:
-        logger.debug("Download started")
-        asyncio.run(download(downloader_input, DATA_DIRECTORY))
+        asyncio.run(download_cli(pairs_list))
     except KeyboardInterrupt:
         print("\nDownload interrupted by user.")
         sys.exit(1)
